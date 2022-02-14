da2e705bf65d9ef3713f41a0af6819a9aabef5f8
adarsh-3099 <65357033+adarsh-3099@users.noreply.github.com>
2022-02-14 09:42:46 +0530

    Update assignment.py

--- AWS/assignment.py
+++ AWS/assignment.py
@@ -1,4 +1,9 @@
 import boto3
+import sys
+
+#print(sys.argv)
+what_to_do = sys.argv[1]
+file_to_perform_action_on = sys.argv[2]
 
 #Creating Session With Boto3.
 session = boto3.Session(
@@ -9,7 +14,6 @@ aws_secret_access_key='TfjaX9cSfnpa8uJX87hkLU4NsWT0Vrs4Ahg/IUXe'
 #Creating S3 Resource From the Session.
 s3 = session.resource('s3')
 
-print(s3)
 
 def upload(file):
     s3 = boto3.client('s3', aws_access_key_id='AKIA22EHG7RB2IAUOKDK',
@@ -42,7 +46,11 @@ def replace(file):
         print('File Could Not Be Replaced')
 
 
-upload('r.txt')
-download('example.txt')
-replace('example.txt')
-download('example.txt')
+if what_to_do == "upload":
+    upload(file_to_perform_action_on)
+
+if what_to_do == "download":
+    download(file_to_perform_action_on)
+
+if what_to_do == "replace":
+    replace(file_to_perform_action_on)
